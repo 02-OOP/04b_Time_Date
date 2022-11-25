1ddd00b52658871a18ebdb68454bfee840e59841
Peter <schmidt.multimedia@gmail.com>
2022-11-25 12:23:45 +0100

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,6 +1,5 @@
 package com.cc.java;
 
-
 // import java.time.LocalDate;
 // import java.time.LocalDateTime;
 // import java.time.LocalTime;
@@ -13,76 +12,76 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		ausgabe("---------- Datum / Ausgaben --------------------");
+		output("---------- Datum / outputn --------------------");
 		
 		// aktuelle Datum
 		LocalDate today = LocalDate.now();
-		ausgabe("Heutiges Datum: " + today);
+		output("Heutiges Datum: " + today);
 		
 		// bestimmtes Datum
 		LocalDate givenDate = LocalDate.of(2012, Month.SEPTEMBER, 21);
-		ausgabe("Bestimmtes Datum: " + givenDate);
+		output("Bestimmtes Datum: " + givenDate);
 		
 		// nächste Woche
 		LocalDate aWeekFromToday = today.plusWeeks(1);
-		ausgabe("Datum in einer Woche: " + aWeekFromToday);
+		output("Datum in einer Woche: " + aWeekFromToday);
 		
 		// gestern
 		LocalDate yesterday = today.minusDays(1);
-		ausgabe("Gestern: " + yesterday);		
+		output("Gestern: " + yesterday);		
 	
 		
-		ausgabe("---------- Vergleiche --------------------");
+		output("---------- Vergleiche --------------------");
 		
-		ausgabe("Schaltjahr?: " + givenDate.isLeapYear() );
-		ausgabe("Heute/Gestern: " + today.isAfter(yesterday) );
-		ausgabe("Gestern/Heute: " + yesterday.isBefore(today));
-		ausgabe("heute/heute: " + today.isEqual(yesterday));
+		output("Schaltjahr?: " + givenDate.isLeapYear() );
+		output("Heute/Gestern: " + today.isAfter(yesterday) );
+		output("Gestern/Heute: " + yesterday.isBefore(today));
+		output("heute/heute: " + today.isEqual(yesterday));
 		
 
-		ausgabe("---------- Extraktion --------------------");
+		output("---------- Extraktion --------------------");
 	
 		int year = today.getYear();
-		ausgabe("aktuelles Jahr: " + year );
+		output("aktuelles Jahr: " + year );
 		
 		givenDate = LocalDate.of(2020, Month.OCTOBER, 9);		
 		String weekday = givenDate.getDayOfWeek().name();
-		ausgabe("Wochentag: " + weekday );
-		ausgabe("Thank God it's " + weekday );
-		ausgabe("Tag im Monat: " + today.getDayOfMonth());
+		output("Wochentag: " + weekday );
+		output("Thank God it's " + weekday );
+		output("Tag im Monat: " + today.getDayOfMonth());
 		
-		ausgabe("---------- Zeit / Ausgaben --------------------");
+		output("---------- Zeit / outputn --------------------");
 		
 		LocalTime justNow = LocalTime.now();
-		ausgabe("Jetzt: " + justNow );
+		output("Jetzt: " + justNow );
 		
 		LocalTime newTime = LocalTime.of(12, 00);
-		ausgabe("Mittag: " + newTime );
+		output("Mittag: " + newTime );
 		
 		LocalTime inOneHour = justNow.plusHours(1);
-		ausgabe("In einer Stunde: " + inOneHour );
+		output("In einer Stunde: " + inOneHour );
 		
-		ausgabe("---------- Extraktion --------------------");
+		output("---------- Extraktion --------------------");
 		
 		int hour = justNow.getHour(); // hora
-		ausgabe("H: " + hour );
+		output("H: " + hour );
 		
 		int minute = justNow.getMinute();
-		ausgabe("MIN: " + minute );
+		output("MIN: " + minute );
 		
-		ausgabe("---------- DateTime --------------------");
+		output("---------- DateTime --------------------");
 		
 		LocalDateTime timeStamp = LocalDateTime.now();
-		ausgabe("timeStamp: " + timeStamp );
+		output("timeStamp: " + timeStamp );
 		
 		System.out.println("---------- Kalenderwoche --------------------");
 		
         Calendar cal = Calendar.getInstance();
-        ausgabe("Kalenderwoche: " + cal.get(Calendar.WEEK_OF_YEAR)); 
+        output("Kalenderwoche: " + cal.get(Calendar.WEEK_OF_YEAR)); 
 
 	}
 
-	public static void ausgabe(String outputStr) {
+	public static void output(String outputStr) {
 		System.out.println(outputStr);  
 	  }
 
